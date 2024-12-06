504f3dc5433bfb04eb65ca204cec873f28a00a53
rajat <fzdsharmarajat@gmail.com>
2024-12-06 13:11:20 +0530

    setup the git repo

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dbConnect = require("./db/dbConnect");
+const userRoutes = require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//call the dbConnect function
+dbConnect();
+
+//Routes
+app.use("/api/users", userRoutes);
+
+module.exports = app;
